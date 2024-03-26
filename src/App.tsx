e898f1fae0e62e32c3a036d0d4588c6ea29035f2
anastasiabarkova <anastasia.barkova@revolve.team>
2024-03-26 20:21:50 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,132 @@
+import * as React from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
+import Breadcrumbs from '@mui/joy/Breadcrumbs';
+import Link from '@mui/joy/Link';
+import Typography from '@mui/joy/Typography';
+
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
+
+import Sidebar from './features/order-dashboard/components/Sidebar';
+import OrderTable from './features/order-dashboard/components/OrderTable';
+import OrderList from './features/order-dashboard/components/OrderList';
+import Header from './features/order-dashboard/components/Header';
+
+export default function JoyOrderDashboardTemplate() {
+  return (
+    <CssVarsProvider disableTransitionOnChange>
+      <CssBaseline />
+      <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
+        <Header />
+        <Sidebar />
+        <Box
+          component="main"
+          className="MainContent"
+          sx={{
+            px: { xs: 2, md: 6 },
+            pt: {
+              xs: 'calc(12px + var(--Header-height))',
+              sm: 'calc(12px + var(--Header-height))',
+              md: 3,
+            },
+            pb: { xs: 2, sm: 2, md: 3 },
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            minWidth: 0,
+            height: '100dvh',
+            gap: 1,
+          }}
+        >
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Breadcrumbs
+              size="sm"
+              aria-label="breadcrumbs"
+              separator={<ChevronRightRoundedIcon fontSize="sm" />}
+              sx={{ pl: 0 }}
+            >
+              <Link
+                underline="none"
+                color="neutral"
+                href="#some-link"
+                aria-label="Home"
+              >
+                <HomeRoundedIcon />
+              </Link>
+              <Link
+                underline="hover"
+                color="neutral"
+                href="#some-link"
+                fontSize={12}
+                fontWeight={500}
+              >
+                Dashboard
+              </Link>
+              <Typography color="primary" fontWeight={500} fontSize={12}>
+                Orders
+              </Typography>
+            </Breadcrumbs>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              mb: 1,
+              gap: 1,
+              flexDirection: { xs: 'column', sm: 'row' },
+              alignItems: { xs: 'start', sm: 'center' },
+              flexWrap: 'wrap',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography level="h2" component="h1">
+              Orders
+            </Typography>
+            <Button
+              color="primary"
+              startDecorator={<DownloadRoundedIcon />}
+              size="sm"
+            >
+              Download PDF
+            </Button>
+          </Box>
+          <OrderTable />
+          <OrderList />
+        </Box>
+      </Box>
+    </CssVarsProvider>
+  );
+}
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB3AUDuPyNkkqPYXGlcJdHRavs2Mi72bXk",
+  authDomain: "abrico-test.firebaseapp.com",
+  projectId: "abrico-test",
+  storageBucket: "abrico-test.appspot.com",
+  messagingSenderId: "456823227665",
+  appId: "1:456823227665:web:f95a3fa924e693a887b8b0",
+  measurementId: "G-Y95R5T88LP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Get a list of cities from your database
+async function getCities(db) {
+  const citiesCol = collection(db, 'cities');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
